8cc67b89efe262286071030ef24c5c15de4d061c
S.M.Jayawardhana <mjayawardhana19240@gmail.com>
2024-01-21 16:08:25 +0530

    My first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace Skills_International
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            pictureBox1 = new PictureBox();
+            label1 = new Label();
+            groupBox1 = new GroupBox();
+            btn_login = new Button();
+            btn_clear = new Button();
+            txt_password = new TextBox();
+            txt_username = new TextBox();
+            label3 = new Label();
+            label2 = new Label();
+            btn_exit = new Button();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            groupBox1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BorderStyle = BorderStyle.FixedSingle;
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(280, 33);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(221, 154);
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 24F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.Location = new Point(203, 209);
+            label1.Name = "label1";
+            label1.Size = new Size(385, 54);
+            label1.TabIndex = 1;
+            label1.Text = "Skills International ";
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(btn_login);
+            groupBox1.Controls.Add(btn_clear);
+            groupBox1.Controls.Add(txt_password);
+            groupBox1.Controls.Add(txt_username);
+            groupBox1.Controls.Add(label3);
+            groupBox1.Controls.Add(label2);
+            groupBox1.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            groupBox1.Location = new Point(130, 308);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(523, 305);
+            groupBox1.TabIndex = 2;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Login";
+            // 
+            // btn_login
+            // 
+            btn_login.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            btn_login.Location = new Point(393, 232);
+            btn_login.Name = "btn_login";
+            btn_login.Size = new Size(94, 45);
+            btn_login.TabIndex = 5;
+            btn_login.Text = "Login";
+            btn_login.UseVisualStyleBackColor = true;
+            btn_login.Click += btn_login_Click;
+            // 
+            // btn_clear
+            // 
+            btn_clear.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            btn_clear.Location = new Point(24, 232);
+            btn_clear.Name = "btn_clear";
+            btn_clear.Size = new Size(94, 45);
+            btn_clear.TabIndex = 4;
+            btn_clear.Text = "Clear";
+            btn_clear.UseVisualStyleBackColor = true;
+            btn_clear.Click += btn_clear_Click;
+            // 
+            // txt_password
+            // 
+            txt_password.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            txt_password.Location = new Point(244, 147);
+            txt_password.Name = "txt_password";
+            txt_password.Size = new Size(243, 34);
+            txt_password.TabIndex = 3;
+            txt_password.UseSystemPasswordChar = true;
+            // 
+            // txt_username
+            // 
+            txt_username.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            txt_username.Location = new Point(244, 62);
+            txt_username.Name = "txt_username";
+            txt_username.Size = new Size(243, 34);
+            txt_username.TabIndex = 2;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            label3.Location = new Point(24, 147);
+            label3.Name = "label3";
+            label3.Size = new Size(101, 28);
+            label3.TabIndex = 1;
+            label3.Text = "Password";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            label2.Location = new Point(19, 62);
+            label2.Name = "label2";
+            label2.Size = new Size(106, 28);
+            label2.TabIndex = 0;
+            label2.Text = "Username";
+            // 
+            // btn_exit
+            // 
+            btn_exit.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            btn_exit.Location = new Point(30, 694);
+            btn_exit.Name = "btn_exit";
+            btn_exit.Size = new Size(94, 45);
+            btn_exit.TabIndex = 5;
+            btn_exit.Text = "Exit";
+            btn_exit.UseVisualStyleBackColor = true;
+            btn_exit.Click += btn_exit_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 765);
+            Controls.Add(btn_exit);
+            Controls.Add(groupBox1);
+            Controls.Add(label1);
+            Controls.Add(pictureBox1);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Login";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private PictureBox pictureBox1;
+        private Label label1;
+        private GroupBox groupBox1;
+        private TextBox txt_username;
+        private Label label3;
+        private Label label2;
+        private TextBox txt_password;
+        private Button btn_clear;
+        private Button btn_login;
+        private Button btn_exit;
+    }
+}
---
+++ Form2.cs
@@ -0,0 +1,290 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.ListView;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.StartPanel;
+using System.Collections;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+
+namespace Skills_International
+{
+    public partial class Form2 : Form
+    {
+        string url = "Data Source=MSI;Initial Catalog=Student;Integrated Security=True";  //server name
+        public Form2()
+        {
+            InitializeComponent();
+            PopulateComboBox();
+            ClearFormData();
+        }
+
+        private void PopulateComboBox() //combobox populate method
+        {
+            comboBox1.Items.Add(1);
+            comboBox1.Items.Add(2);
+            comboBox1.Items.Add(3);
+            comboBox1.Items.Add(4);
+            comboBox1.Items.Add(5);
+            comboBox1.Items.Add(6);
+            comboBox1.Items.Add(7);
+            comboBox1.Items.Add(8);
+            comboBox1.Items.Add(9);
+            comboBox1.Items.Add(10);
+
+        }
+
+        private void ClearFormData()  //data clear method
+        {
+            txt_fname.Clear();
+            txt_lname.Clear();
+            txt_address.Clear();
+            txt_email.Clear();
+            txt_mphone.Clear();
+            txt_hphone.Clear();
+            txt_parentName.Clear();
+            txt_nic.Clear();
+            txt_pphone.Clear();
+            radioButton1.Checked = false;
+            radioButton2.Checked = false;
+        }
+
+
+        private void btn_clear_Click(object sender, EventArgs e)  //clear button
+        {
+            ClearFormData();
+        }
+
+        private void lable_exit_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)  //exit link label
+        {
+            DialogResult result = MessageBox.Show("Are you sure, Do you really want to Exit...?", "Exit", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (result == DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+        }
+
+        private void lable_logout_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)  //logout link label
+        {
+            Form1 form2 = new Form1();
+            form2.Show();
+            this.Hide();
+        }
+
+        private void btn_register_Click(object sender, EventArgs e)  //register button
+        {
+
+            using (SqlConnection connection = new SqlConnection(url))
+            {
+                connection.Open();
+                //data insertion query
+                string query = "INSERT INTO Registration (regNo, firstName, lastName, dateOfBirth, gender, address, email, mobilephone, homePhone, parentName, nic, contactNo) " +
+                                   "VALUES (@regNo, @firstName, @lastName, @dateOfBirth, @gender, @address, @email, @mobilephone, @homePhone, @parentName, @nic, @contactNo)";
+
+                try
+                {
+                    using (SqlCommand command = new SqlCommand(query, connection))
+                    {
+                        command.Parameters.AddWithValue("@regNo", Convert.ToInt32(comboBox1.SelectedItem));
+                        command.Parameters.AddWithValue("@firstName", txt_fname.Text);
+                        command.Parameters.AddWithValue("@lastName", txt_lname.Text);
+                        command.Parameters.AddWithValue("@dateOfBirth", date.Value);
+                        command.Parameters.AddWithValue("@gender", radioButton1.Checked ? "Male" : "Female");
+                        command.Parameters.AddWithValue("@address", txt_address.Text);
+                        command.Parameters.AddWithValue("@email", txt_email.Text);
+                        command.Parameters.AddWithValue("@mobilePhone", Convert.ToInt32(txt_mphone.Text));
+                        command.Parameters.AddWithValue("@homephone", Convert.ToInt32(txt_hphone.Text));
+                        command.Parameters.AddWithValue("@parentName", txt_parentName.Text);
+                        command.Parameters.AddWithValue("@nic", txt_nic.Text);
+                        command.Parameters.AddWithValue("@contactNo", Convert.ToInt32(txt_pphone.Text));
+
+                        int rowsAffected = command.ExecuteNonQuery();
+
+                        if (rowsAffected > 0)
+                        {
+                            MessageBox.Show("Record added successful", "Register Student", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        }
+                        else
+                        {
+                            MessageBox.Show("Registration failed", "Register Student", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(" " + ex);
+                }
+
+            }
+
+        }
+
+        private void btn_delete_Click(object sender, EventArgs e)  //delete button
+        {
+
+            DialogResult result = MessageBox.Show("Are you sure, Do you really want to Delete this Record...?", "Delete", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (result == DialogResult.Yes)
+            {
+                if (comboBox1.SelectedItem != null)
+                {
+                    using (SqlConnection connection = new SqlConnection(url))
+                    {
+                        try
+                        {
+                            connection.Open();
+
+                            //data delete query
+                            string query = "DELETE FROM Registration WHERE regNo = @regNo";
+
+                            SqlCommand command = new SqlCommand(query, connection);
+                            command.Parameters.AddWithValue("@regNo", Convert.ToInt32(comboBox1.SelectedItem));
+
+                            int rowsAffected = command.ExecuteNonQuery();
+
+                            if (rowsAffected > 0)
+                            {
+                                MessageBox.Show("Record deleted successfully", "delete Student", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                                ClearFormData();
+                            }
+                            else
+                            {
+                                MessageBox.Show("No record found with the selected registration number", "Delete Student", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                            }
+                        }
+                        catch (Exception ex)
+                        {
+                            MessageBox.Show("Error: " + ex.Message);
+                        }
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Please select a record to delete", "Delete Student", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+
+            }
+        }
+
+        private void btn_update_Click(object sender, EventArgs e) //update button
+        {
+            if (comboBox1.SelectedItem != null)
+            {
+                using (SqlConnection connection = new SqlConnection(url))
+                {
+                    try
+                    {
+                        connection.Open();
+
+                        //data update query
+                        string query = "UPDATE Registration SET firstName = @firstName, lastName = @lastName, dateOfBirth = @dateOfBirth, gender = @gender, " +
+                                       "address = @address, email = @email, mobilephone = @mobilephone, homePhone = @homePhone, " +
+                                       "parentName = @parentName, nic = @nic, contactNo = @contactNo WHERE regNo = @regNo";
+
+                        SqlCommand command = new SqlCommand(query, connection);
+
+                        command.Parameters.AddWithValue("@regNo", Convert.ToInt32(comboBox1.SelectedItem));
+                        command.Parameters.AddWithValue("@firstName", txt_fname.Text);
+                        command.Parameters.AddWithValue("@lastName", txt_lname.Text);
+                        command.Parameters.AddWithValue("@dateOfBirth", date.Value);
+                        command.Parameters.AddWithValue("@gender", radioButton1.Checked ? "Male" : "Female");
+                        command.Parameters.AddWithValue("@address", txt_address.Text);
+                        command.Parameters.AddWithValue("@email", txt_email.Text);
+                        command.Parameters.AddWithValue("@mobilephone", Convert.ToInt32(txt_mphone.Text));
+                        command.Parameters.AddWithValue("@homePhone", Convert.ToInt32(txt_hphone.Text));
+                        command.Parameters.AddWithValue("@parentName", txt_parentName.Text);
+                        command.Parameters.AddWithValue("@nic", txt_nic.Text);
+                        command.Parameters.AddWithValue("@contactNo", Convert.ToInt32(txt_pphone.Text));
+
+                        int rowsAffected = command.ExecuteNonQuery();
+
+                        if (rowsAffected > 0)
+                        {
+                            MessageBox.Show("Record updated successfully", "Update Student", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        }
+                        else
+                        {
+                            MessageBox.Show("No record found with the selected registration number", "Update Student", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Error: " + ex.Message);
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please select a record to update", "Update Student", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)  // Combo box as a search function
+        {
+            if (comboBox1.SelectedItem != null) // Check if a registration number is selected
+            {
+                using (SqlConnection connection = new SqlConnection(url))
+                {
+                    try
+                    {
+                        connection.Open();
+
+                        //all data selection query
+                        string query = "SELECT * FROM Registration WHERE regNo = @searchValue";
+
+                        SqlCommand command = new SqlCommand(query, connection);
+                        command.Parameters.AddWithValue("@searchValue", Convert.ToInt32(comboBox1.SelectedItem));
+
+                        SqlDataReader reader = command.ExecuteReader();
+
+                        if (reader.Read())
+                        {
+                            txt_fname.Text = reader["firstName"].ToString();
+                            txt_lname.Text = reader["lastName"].ToString();
+                            date.Value = Convert.ToDateTime(reader["dateOfBirth"]);
+                            string gender = reader["gender"].ToString();
+
+                            if (gender == "Male")
+                                radioButton1.Checked = true;
+                            else if (gender == "Female")
+                                radioButton2.Checked = true;
+
+                            txt_address.Text = reader["address"].ToString();
+                            txt_email.Text = reader["email"].ToString();
+
+                            int mobilephone = Convert.ToInt32(reader["mobilephone"]);
+                            txt_mphone.Text = mobilephone.ToString();
+
+                            int homePhone = Convert.ToInt32(reader["homePhone"]);
+                            txt_hphone.Text = homePhone.ToString();
+
+                            int contactNo = Convert.ToInt32(reader["contactNo"]);
+                            txt_pphone.Text = contactNo.ToString();
+                            txt_parentName.Text = reader["parentName"].ToString();
+                            txt_nic.Text = reader["nic"].ToString();
+
+                        }
+                        else
+                        {
+                            MessageBox.Show("No records found for the selected registration number", "Search Student", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                            ClearFormData();
+                        }
+
+                        reader.Close();
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Error: " + ex.Message);
+                    }
+                }
+            }
+        }
+    }
+}
